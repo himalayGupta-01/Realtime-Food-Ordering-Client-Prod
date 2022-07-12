23546a777d411e4314ba56704834e31567ff317f
himalayGupta-01 <himalay9644@gmail.com>
2022-07-12 20:36:34 +0530

    new server link updation

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api="https://realtime-food-ordering.herokuapp.com/"
+export const api="https://realtime-food-ordering-server.herokuapp.com/"
 export const generatePublicUrl=(filename)=>{
-    return `https://realtime-food-ordering.herokuapp.com//${filename}`;
+    return `https://realtime-food-ordering-server.herokuapp.com//${filename}`;
 }
