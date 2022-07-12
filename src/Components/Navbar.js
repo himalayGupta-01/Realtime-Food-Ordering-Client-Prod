d293dd728fc9c071191ff66ff1fa4e6604d65a25
himalayGupta-01 <himalay9644@gmail.com>
2022-07-12 18:23:41 +0530

    Check for Continous Deployment

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
                             <NavLink exact activeClassName="nav-link active" aria-current="page" to="/">Home</NavLink>
                         </li>
                         <li className=" ml-6">
-                            <NavLink activeClassName="nav-link active" to="/contact">Contact</NavLink>
+                            <NavLink activeClassName="nav-link active" to="/contact">Contact Us</NavLink>
                         </li>
 
                         {auth.authenticate ?
